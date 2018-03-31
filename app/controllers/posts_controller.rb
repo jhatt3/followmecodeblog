81b263e1841d530b1459b7db682a19a6bf46af9e
Jesse Hatton <odloads@live.com>
2018-03-31 17:18:55 -0400

    blog functionality completed and added comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    before_action :find_post, only: [:show, :edit, :update, :destroy]
 
     def index
         @posts = Post.all.order('created_at DESC')
@@ -9,7 +10,6 @@ class PostsController < ApplicationController
     end
 
     def show
-        @post = Post.find(params[:id])
     end
 
     def create
@@ -22,14 +22,12 @@ class PostsController < ApplicationController
     end
 
     def destroy
-        @post = Post.find(params[:id])
         @post.destroy
 
         redirect_to root_path
     end
 
     def update
-        @post = Post.find(params[:id])
 
         if @post.update(post_params)
             redirect_to @post
@@ -39,7 +37,6 @@ class PostsController < ApplicationController
     end
 
     def edit
-        @post = Post.find(params[:id])
     end
 
 
@@ -50,4 +47,9 @@ class PostsController < ApplicationController
     def post_params
         params.require(:post).permit(:title, :author, :content)
     end
+
+    def find_post
+        @post = Post.find(params[:id])
+    end
+
 end
